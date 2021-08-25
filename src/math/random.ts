5bd9073e8767fe4cddd84a1d075b7235a84fd95d
weidehai <243395655@qq.com>
2021-08-25 18:03:43 +0800

    random

--- src/math/random.ts
+++ src/math/random.ts
@@ -1,7 +1,10 @@
-export function  getRandomInt(leftBorder:number,rightBorder:number):number {
-  return Math.floor(Math.random()*rightBorder)+leftBorder
+export function getRandomInt(leftBorder: number, rightBorder: number): number {
+  return Math.floor(Math.random() * rightBorder) + leftBorder;
 }
 
-export function  getRandomFloat(leftBorder:number,rightBorder:number):number {
-  return Math.random()*(rightBorder-1)+leftBorder
+export function getRandomFloat(
+  leftBorder: number,
+  rightBorder: number
+): number {
+  return Math.random() * (rightBorder - 1) + leftBorder;
 }
